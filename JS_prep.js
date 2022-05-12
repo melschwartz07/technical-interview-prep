c5b1045dd87cdf73bf6b6a31e71dfacfde8be091
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-05-12 15:37:34 -0500

    daily commit

--- JS_prep.js
+++ JS_prep.js
@@ -61,3 +61,26 @@ function solution(inputArray) {
 function solution(s) {
 
 }
+
+//______________________________________________________________________________________________________________________________
+
+// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly
+// increasing sequence by removing no more than one element from the array.
+
+function solution(sequence) {
+    var found = 0;
+    for (var i=0;i<sequence.length;i++) {
+
+        if(sequence[i] <= sequence[i-1]) {
+            found++;
+            // check if more than one nonincreasing found
+            if(found > 1) return false;
+
+            // check if second previous number is equal to / bigger than current number
+            // and previous number is equalto / bigger than next number
+            if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) return false;
+        }
+
+    }
+    return true;
+}
