d94579a27feec8a1ad9e6b81a2e86acb55c2c030
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-05-13 11:36:11 -0500

    daily commit

--- JS_prep.js
+++ JS_prep.js
@@ -162,3 +162,32 @@ function solution(s1, s2) {
 }
 
 //______________________________________________________________________________________________________________________________
+// Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the
+// first half of the digits is equal to the sum of the second half.
+
+    // Given a ticket number n, determine if it's lucky or not.
+
+function solution(n) {
+    stringN = n.toString()
+    var first = stringN.substring(0, stringN.length / 2).split("");
+    var second = stringN.substring((stringN.length / 2), stringN.length).split("");
+
+    var firstSum = 0;
+    var secondSum = 0;
+
+    var i = 0;
+
+    for (i = 0; i < first.length; i++) {
+        firstSum += parseInt(first[i]);
+
+        secondSum += parseInt(second[i]);
+    }
+
+    if (firstSum === secondSum) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//______________________________________________________________________________________________________________________________
