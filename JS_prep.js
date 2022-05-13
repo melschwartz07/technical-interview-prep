05da54173f0a46696404a777e954eebaf457c0e8
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-05-13 11:27:41 -0500

    daily commit

--- JS_prep.js
+++ JS_prep.js
@@ -147,3 +147,18 @@ function solution(inputArray) {
 
 //______________________________________________________________________________________________________________________________
 
+// Given two strings, find the number of common characters between them.
+
+function solution(s1, s2) {
+    var count = 0;
+    for(var i = 0; i < s1.length; i++) {
+        var j = s2.indexOf(s1[i]);
+        if(j>=0){
+            count++;
+            s2 = s2.replace(s1[i],'');
+        }
+    }
+    return count;
+}
+
+//______________________________________________________________________________________________________________________________
