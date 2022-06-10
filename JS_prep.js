b2a45c5d4466cba13c67419d60b61886b69d2efd
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-09 20:34:22 -0500

    daily commit

--- JS_prep.js
+++ JS_prep.js
@@ -250,3 +250,24 @@ function solution(s) {
 }
 
 
+// Several people are standing in a row and need to be divided into two teams. The first person goes into team 1,
+// the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+// You are given an array of positive integers - the weights of the people.
+// Return an array of two integers, where the first element is the total weight of team 1, and the second element
+// is the total weight of team 2 after the division is complete.
+
+function solution(a) {
+    var team1 = 0;
+    var team2 = 0;
+
+    for (var i = 0; i < a.length; i++) {
+        if (i % 2 == 0) {
+            team1 += a[i];
+        } else {
+            team2 += a[i];
+        }
+    }
+
+    return [team1, team2];
+}
+
