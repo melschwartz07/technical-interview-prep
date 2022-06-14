e8ee02bad960a80c0fb9dd69d0f29ea64bf403bf
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-13 19:29:53 -0500

    daily commit

--- JS_prep.js
+++ JS_prep.js
@@ -352,5 +352,17 @@ function solution(inputString) {
 
 //______________________________________________________________________________________________________________________________
 
+// Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+//
+//     Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+//
+//     Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
+
+    return yourLeft == friendsLeft && yourRight == friendsRight || yourLeft == friendsRight && yourRight == friendsLeft;
+}
+
+
 
 
