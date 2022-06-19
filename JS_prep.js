5cae0be10f68a4c13c7e3bf8d9100ee980c6bbcf
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-18 21:20:38 -0500

    daily commit

--- JS_prep.js
+++ JS_prep.js
@@ -384,6 +384,21 @@ function solution(nums) {
 }
 //______________________________________________________________________________________________________________________________
 
+// Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+function solution(nums) {
+    var max = 0;
+
+    for (var i = 1; i < nums.length; i++) {
+        var diff = Math.abs(nums[i] - nums[i - 1]);
+        max = (diff > max) ? diff : max;
+    }
+
+    return max;
+}
+
+
+
 
 
 
